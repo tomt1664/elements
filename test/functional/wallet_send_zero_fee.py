3919452371775b990363a0d58d5a99048c6cb756
Tom Trevethan <ttrevethan@blockstream.com>
2025-07-24 13:42:12 +0100

    update zero fee test

--- test/functional/wallet_send_zero_fee.py
+++ test/functional/wallet_send_zero_fee.py
@@ -15,16 +15,11 @@ class WalletTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 3
         self.extra_args = [[
-            "-blindedaddresses=1",
-            "-initialfreecoins=2100000000000000",
-            "-con_blocksubsidy=0",
-            "-con_connect_genesis_outputs=1",
             "-txindex=1",
             "-minrelaytxfee=0",
             "-blockmintxfee=0",
             "-mintxfee=0",
         ]] * self.num_nodes
-        self.extra_args[0].append("-anyonecanspendaremine=1")
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
@@ -33,15 +28,12 @@ class WalletTest(BitcoinTestFramework):
         self.generate(self.nodes[0], COINBASE_MATURITY + 10)
         self.sync_all()
 
-        balance = self.nodes[0].getbalance()
-        assert_equal(balance["bitcoin"],21000000)
-
         new_addr = self.nodes[1].getnewaddress()
         self.nodes[0].sendtoaddress(new_addr, 10)
         self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 20)
         self.generate(self.nodes[0], 1)
         assert_equal(self.nodes[0].getblockchaininfo()["blocks"], 111)
-        assert_equal(self.nodes[0].getbalance(), {'bitcoin': Decimal('20999969.99900900')})
+        assert_equal(self.nodes[0].getbalance(), {'bitcoin': Decimal('519.99986640')})
         assert_equal(self.nodes[1].getbalance(), {'bitcoin': Decimal('10.00000000')})
         assert_equal(self.nodes[2].getbalance(), {'bitcoin': Decimal('20.00000000')})
 
@@ -51,13 +43,14 @@ class WalletTest(BitcoinTestFramework):
         assert_equal(tx["fee"]["bitcoin"], 0)
         hex = self.nodes[0].getrawtransaction(txid)
         self.generate(self.nodes[0], 1)
+        self.sync_all()
         assert_equal(self.nodes[0].getblockchaininfo()["blocks"], 112)
 
         decoded = self.nodes[0].decoderawtransaction(hex)
         # there should be no fee output
         assert_equal(decoded["fee"], {})
         assert_equal(len(decoded["vout"]),2)
-        assert_equal(self.nodes[0].getbalance(), {'bitcoin': Decimal('20999968.99900900')})
+        assert_equal(self.nodes[0].getbalance(), {'bitcoin': Decimal('568.99986640')})
         assert_equal(self.nodes[1].getbalance(), {'bitcoin': Decimal('11.00000000')})
         assert_equal(self.nodes[2].getbalance(), {'bitcoin': Decimal('20.00000000')})
 
